608fad93b1d99a00ca6f3ee50436f32e0d2c594a
Zosia <korczzofia@gmail.com>
2020-10-28 12:39:49 +0100

    level selection on menu overlay

--- src/js/components/MenuOverlay.tsx
+++ src/js/components/MenuOverlay.tsx
@@ -36,6 +36,63 @@ const MenuOverlay: React.FunctionComponent<Props> = ({
                     <button className="button next" onClick={onGameRestart}>
                         New game
                     </button>
+                    <div className="levels-display">
+                        <p className="levels-header">Levels</p>
+                        <div className="level">
+                            <p className="level-no">1</p>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">2</p>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">3</p>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">4</p>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">5</p>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">6</p>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">7</p>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">8</p>
+                            <i className="fas fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+                        <div className="level">
+                            <p className="level-no">9</p>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                            <i className="far fa-star"></i>
+                        </div>
+				    </div>
 				</div>
                 <p className="credits">
                     © 2020 made by <a href="http://zofiakorcz.pl" className="credits-link" target="_blank">
