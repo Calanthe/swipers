d18d368599544a766e77f2c64e4624f8f8d8176c
Zosia <korczzofia@gmail.com>
2020-12-07 12:28:29 +0100

    overlays positions are fixed

--- src/js/components/MenuOverlay.tsx
+++ src/js/components/MenuOverlay.tsx
@@ -35,51 +35,53 @@ const MenuOverlay: React.FunctionComponent<Props> = ({
     onSetLevel,
     onToggleHints
 }) => {
-	const menuOverlayClass = classNames("menu-overlay", {
+	const menuOverlayClass = classNames("menu-overlay-wrapper", {
 			visible: isMenuVisible,
 		});
 
 	return (
         <div className={menuOverlayClass}>
-            <h1 className="logo">Swipers</h1>
-            <div className="content-wrapper">
-                {level > 0 || moves > 0 ? (
-                    <p>
-                        <button className="button" onClick={onCloseMenu}>
-                            <i className="fas fa-long-arrow-alt-left"></i> Back to game
+            <div className="menu-overlay">
+                <h1 className="logo">Swipers</h1>
+                <div className="content-wrapper">
+                    {level > 0 || moves > 0 ? (
+                        <p>
+                            <button className="button" onClick={onCloseMenu}>
+                                <i className="fas fa-long-arrow-alt-left"></i> Back to game
+                            </button>
+                            <button className="button" onClick={onLevelRestart}>
+                                <i className="fas fa-undo"></i> Restart level
+                            </button>
+                        </p>
+                    ) : (
+                        <button className="button" onClick={onGameRestart}>
+                            New game
                         </button>
-                        <button className="button" onClick={onLevelRestart}>
-                            <i className="fas fa-undo"></i> Restart level
+                    )}
+                    {level > 0 || moves > 0 ? (
+                        <button className="button" onClick={onGameRestart}>
+                            New game
                         </button>
-                    </p>
-                ) : (
-                    <button className="button" onClick={onGameRestart}>
-                        New game
-                    </button>
-				)}
-                {level > 0 || moves > 0 ? (
-                    <button className="button" onClick={onGameRestart}>
-                        New game
-                    </button>
-				) : null}  
-                {isHintsVisible ? (
-                    <button className="button hints" onClick={onToggleHints}>
-                        <span>Turn off hints</span> 
-                        <i className="fas fa-toggle-on"></i>
-                    </button>
-                ) : (
-                    <button className="button hints" onClick={onToggleHints}>
-                        <span>Turn on hints</span> 
-                        <i className="fas fa-toggle-off"></i>
-                    </button>
-                )}
-                <LevelsDisplay onSetLevel={onSetLevel}/>
+                    ) : null}  
+                    {isHintsVisible ? (
+                        <button className="button hints" onClick={onToggleHints}>
+                            <span>Turn off hints</span> 
+                            <i className="fas fa-toggle-on"></i>
+                        </button>
+                    ) : (
+                        <button className="button hints" onClick={onToggleHints}>
+                            <span>Turn on hints</span> 
+                            <i className="fas fa-toggle-off"></i>
+                        </button>
+                    )}
+                    <LevelsDisplay onSetLevel={onSetLevel}/>
+                </div>
+                <p className="credits">
+                    © 2020 <a href="http://zofiakorcz.pl" className="credits-link" target="_blank" rel="noopener noreferrer">
+                        Zofia Korcz
+                    </a>
+                </p>
             </div>
-            <p className="credits">
-                © 2020 <a href="http://zofiakorcz.pl" className="credits-link" target="_blank" rel="noopener noreferrer">
-                    Zofia Korcz
-                </a>
-            </p>
         </div>
 	);
 };
