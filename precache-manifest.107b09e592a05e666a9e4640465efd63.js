a72a3923fe7d6505d71384409713ba3249c5a963
Zosia <korczzofia@gmail.com>
2020-12-13 13:40:15 +0100

    Updates

---
+++ precache-manifest.107b09e592a05e666a9e4640465efd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd5f13d093acbb17a039f770cbdf6fb",
+    "url": "/swipers/index.html"
+  },
+  {
+    "revision": "ff5a0534913421494e5d",
+    "url": "/swipers/static/css/main.d1265588.chunk.css"
+  },
+  {
+    "revision": "544f4d4f493cdeb969b6",
+    "url": "/swipers/static/js/2.3d928302.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/swipers/static/js/2.3d928302.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff5a0534913421494e5d",
+    "url": "/swipers/static/js/main.40e7a138.chunk.js"
+  },
+  {
+    "revision": "27959af1fde352a27e28",
+    "url": "/swipers/static/js/runtime-main.e34253b7.js"
+  }
+]);
