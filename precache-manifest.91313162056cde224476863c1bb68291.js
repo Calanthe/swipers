86a40828aeb893536f290d745de03544e33d30c6
Zosia <korczzofia@gmail.com>
2020-12-13 13:22:51 +0100

    Updates

--- precache-manifest.91313162056cde224476863c1bb68291.js
+++ precache-manifest.91313162056cde224476863c1bb68291.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "791b1c97ae68fd924e8cb55934123704",
-    "url": "/swipers/index.html"
-  },
-  {
-    "revision": "89c3af04c689ea6c6cf1",
-    "url": "/swipers/static/css/main.fec003ca.chunk.css"
-  },
-  {
-    "revision": "6049c82061cbfbe8d6b4",
-    "url": "/swipers/static/js/2.46190b77.chunk.js"
-  },
-  {
-    "revision": "11da4d0289ce192632a3fc6590a86fde",
-    "url": "/swipers/static/js/2.46190b77.chunk.js.LICENSE"
-  },
-  {
-    "revision": "89c3af04c689ea6c6cf1",
-    "url": "/swipers/static/js/main.235e54c4.chunk.js"
-  },
-  {
-    "revision": "27959af1fde352a27e28",
-    "url": "/swipers/static/js/runtime-main.e34253b7.js"
-  }
+	{
+		revision: "791b1c97ae68fd924e8cb55934123704",
+		url: "/swipers/index.html",
+	},
+	{
+		revision: "89c3af04c689ea6c6cf1",
+		url: "/swipers/static/css/main.fec003ca.chunk.css",
+	},
+	{
+		revision: "6049c82061cbfbe8d6b4",
+		url: "/swipers/static/js/2.46190b77.chunk.js",
+	},
+	{
+		revision: "11da4d0289ce192632a3fc6590a86fde",
+		url: "/swipers/static/js/2.46190b77.chunk.js.LICENSE",
+	},
+	{
+		revision: "89c3af04c689ea6c6cf1",
+		url: "/swipers/static/js/main.235e54c4.chunk.js",
+	},
+	{
+		revision: "27959af1fde352a27e28",
+		url: "/swipers/static/js/runtime-main.e34253b7.js",
+	},
 ]);
--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/swipers/precache-manifest.91313162056cde224476863c1bb68291.js"
+	"https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+importScripts("/swipers/precache-manifest.91313162056cde224476863c1bb68291.js");
+
+self.addEventListener("message", (event) => {
+	if (event.data && event.data.type === "SKIP_WAITING") {
+		self.skipWaiting();
+	}
 });
 
 workbox.core.clientsClaim();
@@ -33,7 +33,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swipers/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+	workbox.precaching.getCacheKeyForURL("/swipers/index.html"),
+	{
+		blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
+	}
+);
